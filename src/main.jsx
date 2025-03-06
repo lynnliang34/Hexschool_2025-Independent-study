5928a66bfacc925bc3cc35c54bfb14739b77039e
lynnliang34 <lynnliang34@gmail.com>
2025-03-06 10:44:23 +0800

    feat: 使用 store - 在 main.js 設定 Redux Provider
    1. 在 main.jsx 引入 Provider
    2. 匯入 store
    3. 使用 store 作為 Provider 的 props 傳入

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./routes";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
