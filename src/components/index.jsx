dc64d1b7037fffeac0947479d16d78b886798225
lynnliang34 <lynnliang34@gmail.com>
2025-03-08 11:55:40 +0800

    feat: 建立 toastSlice、Toast 元件 (第七周作業)
    1. 複製第七周作業 toastSlice.js、Toast.jsx
    2. src > redux > toastSlice.js
    3. src > components > Toast.jsx
    4. src > components > index 集中管理元件

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,3 +6,6 @@ export { default as HeaderNavbar } from "./HeaderNavbar";
 // SVG
 export { default as Logo } from "./Logo";
 export { default as LogoSlogan } from "./LogoSlogan";
+
+// 提示訊息
+export { default as Toast } from "./Toast";
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
 import adminReducer from "./adminSlice";
+import toastReducer from "./toastSlice";
 
 const store = configureStore({
   reducer: {
     user: useReducer,
     admin: adminReducer,
+    toast: toastReducer,
   },
 });
 
