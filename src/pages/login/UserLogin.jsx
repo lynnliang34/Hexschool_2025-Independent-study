a5239710d08d10b6fb31dfecc44e28b8f46e73c0
lynnliang34 <lynnliang34@gmail.com>
2025-03-08 12:01:15 +0800

    feat: 使用者登入失敗跳 Toast 訊息
    1. 引入 Toast 元件
    2. 引入 toastSlice 的 pushMessage 方法
    3. 版面加 <Toast />
    4. 登入失敗 alert 改 dispatch( pushMessage({ }) )

--- src/pages/login/UserLogin.jsx
+++ src/pages/login/UserLogin.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../redux/userSlice";
-import { Logo } from "../../components";
+import { Logo, Toast } from "../../components";
+import { pushMessage } from "../../redux/toastSlice";
 
 // 環境變數
 const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -42,47 +43,56 @@ export default function UserLogin() {
       dispatch(loginUser({ name: account.username }));
       navigate("/profile");
     } catch (error) {
-      alert(`登入失敗：${error.message}`);
+      dispatch(
+        pushMessage({
+          text: `登入失敗：${error.message}`,
+          status: "failed",
+        })
+      );
     }
   };
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-primary-2">
-      <div className="d-flex align-items-center">
-        <Link to="/">
-          <Logo className={"login-logo me-1"} />
-        </Link>
-        <h1 className="mt-5 mb-7 me-1">會員登入</h1>
+    <>
+      <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-primary-2">
+        <div className="d-flex align-items-center">
+          <Link to="/">
+            <Logo className={"login-logo me-1"} />
+          </Link>
+          <h1 className="mt-5 mb-7 me-1">會員登入</h1>
+        </div>
+
+        <form onSubmit={handleLogin} className="d-flex flex-column gap-3 ">
+          <div className="form-floating mb-1">
+            <input
+              name="username"
+              value={account.username}
+              onChange={handleInputChange}
+              type="email"
+              className="form-control"
+              id="username"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="username">Email address</label>
+          </div>
+          <div className="form-floating mb-4">
+            <input
+              name="password"
+              value={account.password}
+              onChange={handleInputChange}
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <button className="btn btn-primary text-white">登入</button>
+        </form>
+        <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
       </div>
 
-      <form onSubmit={handleLogin} className="d-flex flex-column gap-3 ">
-        <div className="form-floating mb-1">
-          <input
-            name="username"
-            value={account.username}
-            onChange={handleInputChange}
-            type="email"
-            className="form-control"
-            id="username"
-            placeholder="name@example.com"
-          />
-          <label htmlFor="username">Email address</label>
-        </div>
-        <div className="form-floating mb-4">
-          <input
-            name="password"
-            value={account.password}
-            onChange={handleInputChange}
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-          />
-          <label htmlFor="password">Password</label>
-        </div>
-        <button className="btn btn-primary text-white">登入</button>
-      </form>
-      <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
-    </div>
+      <Toast />
+    </>
   );
 }
