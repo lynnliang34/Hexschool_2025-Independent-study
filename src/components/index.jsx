ed90300c15e3375006f34015f3116a3b5078a9a5
lynnliang34 <lynnliang34@gmail.com>
2025-03-08 09:28:07 +0800

    feat: 拆出 SVG 元件 Logo、LogoSlogan
    1. 拉出 <svg> 元件放 src > components 資料夾
    2. src > components > index 集中管理元件
    3. 在 Header.jsx 用解構方式取用元件

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,7 @@
 export { default as HeaderSearchBar } from "./HeaderSearchBar";
 export { default as HeaderSidebarMenu } from "./HeaderSidebarMenu";
 export { default as HeaderNavbar } from "./HeaderNavbar";
+
+// SVG
+export { default as Logo } from "./Logo";
+export { default as LogoSlogan } from "./LogoSlogan";
