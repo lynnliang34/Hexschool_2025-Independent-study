573cca0a8b5d3f95dcae79648e69f9b7c4bbaa1b
lynnliang34 <lynnliang34@gmail.com>
2025-03-10 00:18:13 +0800

    feat: 複製 建立/編輯/刪除 產品頁面與功能 (第四周作業)

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,57 @@
+function Pagination({ pageInfo, handlePageChange }) {
+  const page = pageInfo.current_page;
+
+  return (
+    <div className="d-flex justify-content-center">
+      <nav>
+        <ul className="pagination">
+          <li className={`page-item ${!pageInfo.has_pre && "disabled"}`}>
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                handlePageChange(page - 1);
+              }}
+              className="page-link"
+              href="#"
+            >
+              上一頁
+            </a>
+          </li>
+
+          {Array.from({ length: pageInfo.total_pages }).map((_, index) => (
+            <li
+              className={`page-item ${page === index + 1 && "active"}`}
+              key={index}
+            >
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  handlePageChange(index + 1);
+                }}
+                className="page-link"
+                href="#"
+              >
+                {index + 1}
+              </a>
+            </li>
+          ))}
+
+          <li className={`page-item ${!pageInfo.has_next && "disabled"}`}>
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                handlePageChange(page + 1);
+              }}
+              className="page-link"
+              href="#"
+            >
+              下一頁
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Pagination;
