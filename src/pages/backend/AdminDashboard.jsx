87d60dcc1f4ffe230fffffd4c8b111470d82ddd7
lynnliang34 <lynnliang34@gmail.com>
2025-03-14 15:12:06 +0800

    feat: 〔後台〕首頁顯示學員人數、訂單數、營業額切版

--- src/pages/backend/AdminDashboard.jsx
+++ src/pages/backend/AdminDashboard.jsx
@@ -1,7 +1,36 @@
 export default function AdminDashboard() {
   return (
     <div className="container">
-      <h1>後台首頁 (儀表版)</h1>
+      <h1 className="fw-bold text-secondary">後台首頁</h1>
+      <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 my-6">
+        <div className="col mb-6">
+          <div className="bg-primary-2 rounded-20 h-100">
+            <h2 className="fs-1 p-6">學員人數</h2>
+            <div className=" text-end p-6">
+              <p className="fs-1 text-primary-2">Students</p>
+              <p className="fs-0">123</p>
+            </div>
+          </div>
+        </div>
+        <div className="col mb-6">
+          <div className="bg-primary-2 rounded-20 h-100">
+            <h2 className="fs-1 p-6">課程訂單數</h2>
+            <div className=" text-end p-6">
+              <p className="fs-1 text-primary-2">Orders</p>
+              <p className="fs-0">123</p>
+            </div>
+          </div>
+        </div>
+        <div className="col mb-6">
+          <div className="bg-primary-2 rounded-20 h-100">
+            <h2 className="fs-1 p-6">營業額</h2>
+            <div className=" text-end p-6">
+              <p className="fs-1">NT$</p>
+              <p className="fs-0">123,000</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
