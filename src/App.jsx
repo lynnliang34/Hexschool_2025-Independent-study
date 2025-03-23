b64c56f8dbbd50e385ba66aa0d7815db1df67dbb
Anna <s12345abcd5@gmail.com>
2025-03-23 17:44:31 +0800

    feat:切換畫面置頂

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import { Header, Footer } from "./pages";
+import ScrollToTop from "./helpers/ScrollToTop";
 
 function App({}) {
   return (
     <>
       <Header />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </>
