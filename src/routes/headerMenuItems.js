d6f5b438d7a7b066244c2eb169cbe2219c1c6906
lynnliang34 <lynnliang34@gmail.com>
2025-04-13 20:24:08 +0800

    fix: 導覽列 - 移除 關於我們、知識分享、活動照片 子分類

--- src/routes/headerMenuItems.js
+++ src/routes/headerMenuItems.js
@@ -2,10 +2,6 @@ const headerMenuItems = [
   {
     title: "關於我們",
     link: "/about",
-    subItems: [
-      { title: "師資", link: "/about#teachers" },
-      { title: "交通資訊", link: "/about#transport" },
-    ],
   },
   {
     title: "探索課程",
@@ -20,23 +16,10 @@ const headerMenuItems = [
   {
     title: "知識分享",
     link: "/knowledge-sharing",
-    subItems: [
-      { title: "運動", link: "/knowledge-sharing#sport" },
-      { title: "飲食", link: "/knowledge-sharing#food" },
-      { title: "養生", link: "/knowledge-sharing#health" },
-      { title: "醫療", link: "/knowledge-sharing#medical" },
-      { title: "癌症", link: "/knowledge-sharing#cancer" },
-      { title: "居家", link: "/knowledge-sharing#home" },
-    ],
   },
   {
     title: "活動照片",
     link: "/photos",
-    subItems: [
-      { title: "講座", link: "/photos#lecture" },
-      { title: "社團", link: "/photos#club" },
-      { title: "運動會", link: "/photos#sportsevent" },
-    ],
   },
 ];
 
