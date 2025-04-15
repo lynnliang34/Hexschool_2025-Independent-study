1b87f2e030d3a1d0d999ea8bf3db26263960d7b6
lynnliang34 <lynnliang34@gmail.com>
2025-04-15 11:31:19 +0800

    feat: 忘記密碼頁 - 按重設密碼後的畫面

--- src/pages/login/UserForgotPassword.jsx
+++ src/pages/login/UserForgotPassword.jsx
@@ -19,40 +19,67 @@ export default function UserLogin() {
     });
   };
 
+  // 是否按重設密碼鈕
+  const [resetPassword, setResetPassword] = useState(false);
+
+  const handleResetPassword = (e) => {
+    e.preventDefault();
+    setResetPassword(true);
+  };
+
   return (
     <>
       <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-primary-2">
         <Link to="/">
           <Logo className={"login-logo"} />
         </Link>
-        <h1 className="mt-5 mb-7">忘記密碼</h1>
+        {resetPassword ? (
+          <>
+            <div className="d-flex flex-column">
+              <h3 className="mt-4 mb-8">已發送信件至您的電子信箱</h3>
+              <Link to="/login" className="text-center">
+                <button className="btn btn-primary text-white px-10">
+                  去登入
+                </button>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className="mt-5 mb-7">忘記密碼</h1>
+            <form
+              onSubmit={handleResetPassword}
+              className="d-flex flex-column gap-3 login-input-w"
+            >
+              <div className="form-floating mb-4">
+                <input
+                  name="username"
+                  value={account.username}
+                  onChange={handleInputChange}
+                  type="email"
+                  className="form-control "
+                  id="username"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="username">電子信箱</label>
+              </div>
 
-        <form className="d-flex flex-column gap-3 login-input-w">
-          <div className="form-floating mb-4">
-            <input
-              name="username"
-              value={account.username}
-              onChange={handleInputChange}
-              type="email"
-              className="form-control "
-              id="username"
-              placeholder="name@example.com"
-            />
-            <label htmlFor="username">電子信箱</label>
-          </div>
-
-          <button className="btn btn-primary text-white mx-10">重設密碼</button>
-        </form>
-        <p className="mt-5 mb-3 fs-7">
-          沒有帳號？
-          <Link to="/register">
-            <span className="login-links">註冊去</span>
-          </Link>
-          <span className="mx-1"> | </span>
-          <Link to="/">
-            <span className="login-links">回首頁</span>
-          </Link>
-        </p>
+              <button className="btn btn-primary text-white mx-10">
+                重設密碼
+              </button>
+            </form>
+            <p className="mt-5 mb-3 fs-7">
+              沒有帳號？
+              <Link to="/register">
+                <span className="login-links">註冊去</span>
+              </Link>
+              <span className="mx-1"> | </span>
+              <Link to="/">
+                <span className="login-links">回首頁</span>
+              </Link>
+            </p>
+          </>
+        )}
       </div>
     </>
   );
