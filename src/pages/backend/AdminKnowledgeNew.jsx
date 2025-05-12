041b8397ab272280d3de67074e3c4a638947573f
Anna <s12345abcd5@gmail.com>
2025-05-12 15:17:05 +0800

    feat: 後台-知識分享(Anna) - 取得文章資料，帶入表單

--- src/pages/backend/AdminKnowledgeNew.jsx
+++ src/pages/backend/AdminKnowledgeNew.jsx
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_PATH = import.meta.env.VITE_API_PATH;
+
 export default function AdminKnowledgeNew(){
+  const [ articleList, setArticleList ] = useState([]);
+
+  useEffect(()=>{
+    const getAllArticle = async()=>{
+      const res = await axios.get(`${BASE_URL}/api/${API_PATH}/admin/articles`,{
+        page:1
+      });
+      setArticleList(res.data.articles)
+    }
+    getAllArticle();
+    
+    console.log(articleList);
+  },[])
+
+
+  
+
   return (<>
     <div className="container">
       <div className="row">
@@ -18,12 +41,21 @@ export default function AdminKnowledgeNew(){
               </tr>
             </thead>
             <tbody>
+              {articleList.map((article)=>(
               <tr>
-                <td>文章名稱</td>
-                <td>發布日期</td>
-                <td>是否發布</td>
-                <td>編輯 / 刪除</td>
+                <td>{article.title}</td>
+                <td>{new Date(article.create_at* 1000).toLocaleDateString()}</td>
+                <td>{article.isPublic ? <span className="text-secondary">已發布</span>: '未發布'}</td>
+                <td>
+                  <button type="button" className="edit-product-btn">
+                  <i className="bi bi-pencil-square fs-4"></i>
+                  </button>
+                  <button type="button" className="edit-product-btn">
+                  <i className="bi bi-trash3-fill fs-4"></i>
+                  </button>
+                </td>
               </tr>
+              ))}
             </tbody>
           </table>
         </div>
